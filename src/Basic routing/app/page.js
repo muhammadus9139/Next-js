7f8db1caef5a0ff6fd0faf5d2bf1c832aa86a554
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-18 19:20:18 +0500

    Create page.js

---
+++ src/Basic routing/app/page.js
@@ -0,0 +1,26 @@
+'use client'; // client components like onclick ko use krny k liye use client likhna ha q k ye client side components hain or next js server pr b render hota ha
+import { useState } from "react";
+
+export default function Home() {
+
+    const [name,setname]=useState("ali");
+    const apple = () => {
+        setname("ahmad");
+    }
+
+    const Innercomp=()=>{
+        return(
+            <h1>inner component</h1>
+        )
+    }
+
+    return (
+        <main>
+            <h1>Root page {name}</h1>
+            <button onClick={()=>apple()}>click me</button>
+            <Innercomp/> // component call
+            {Innercomp()} // function call
+        </main>
+    );
+}
+

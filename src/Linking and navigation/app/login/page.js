ea021f8e9157005672edcfe33bcb3e62a406b65a
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-20 12:42:45 +0500

    Create page.js

---
+++ src/Linking and navigation/app/login/page.js
@@ -0,0 +1,13 @@
+'use client';
+import Link from "next/link";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <Link href="/">Go to Home</Link>
+    </div>
+  );
+};
+
+export default Login;

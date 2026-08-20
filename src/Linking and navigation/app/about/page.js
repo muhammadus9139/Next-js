b1d1e08128ac9e37ce57ef2309f85d90a6873998
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-20 12:43:10 +0500

    Create page.js

---
+++ src/Linking and navigation/app/about/page.js
@@ -0,0 +1,14 @@
+
+'use client';
+import Link from "next/link";
+
+const About=()=>{
+    return(
+        <div>
+            <h1>About page</h1> 
+            <Link href="/">Go to Home</Link>
+        </div>
+    )
+}
+
+export default About;

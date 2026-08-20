7312cc6b55ceaf6c234bf67bd88177f86edb746f
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-20 16:02:45 +0500

    Create page.js

---
+++ src/404 page in next js/app/about/[...not-found]/page.js
@@ -0,0 +1,12 @@
+// this is specific route of not-found
+// this is for about page
+
+'use client'
+
+export default function AboutNotfound(){
+    return(
+        <div>
+            <h1>This about page Not Found</h1>
+        </div>
+    )
+}

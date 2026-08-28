1cab1cc24337ae5b6cf85465358896e44deef82b
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-28 15:01:01 +0500

    Create Home component for client-side navigation
    Implemented a client component for navigation and data fetching.

---
+++ src/Fetch data in client component/app/page.js
@@ -0,0 +1,21 @@
+'use client'; // client components like onclick ko use krny k liye use client likhna ha q k ye client side components hain or next js server pr b render hota ha
+import { useState } from "react";
+import Link from "next/link";
+import {useRouter} from "next/navigation";
+
+export default function Home() {
+
+    const router = useRouter(); // for button click navigation
+
+      function navigate(name) {
+          router.push(name)
+     }
+
+    return (
+        <main>
+            <h1>Root page</h1> 
+            <Link href="/studentlist">Go to student list</Link>
+        </main>
+    );
+}
+

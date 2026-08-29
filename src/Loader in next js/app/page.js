486122db3b84520694b9c1eacb38135e3b0da48c
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-29 13:48:38 +0500

    Create page.js

---
+++ src/Loader in next js/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import styles from '../style.module.css';
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Home page</h1>
+    </main>
+  );
+}

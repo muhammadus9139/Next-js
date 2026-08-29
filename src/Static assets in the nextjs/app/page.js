cfe716391e6c3c53349fbec6c33d5d79fd7a4bbe
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-29 14:08:15 +0500

    Add main page with image and styles

---
+++ src/Static assets in the nextjs/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from '../style.module.css';
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Home page</h1>
+      <img src="/example.svg"/>
+    </main>
+  );
+}

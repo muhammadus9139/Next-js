59e0bebbcf34fe1245e5e067d935ab3a626a8a85
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-30 15:41:19 +0500

    Create page.js

---
+++ src/Export static html page with build/app/seller/page.js
@@ -0,0 +1,11 @@
+import User from "../components/user";
+
+
+export default function SellerPage(){
+    return (
+        <div>
+            <h1>Seller Product page</h1>
+            <User/>
+        </div>
+    )
+}

e14c0fb79587f05b96327236ad77845b54e0784e
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-30 19:33:38 +0500

    Create page.js

---
+++ src/Redirection in next js/app/user/[userId]/page.js
@@ -0,0 +1,11 @@
+
+import {redirect } from 'next/navigation'
+
+export default function Page() {
+    // redirect("/about")
+  return (
+    <main>
+      <h1>User detail page</h1>
+    </main>
+  );
+}

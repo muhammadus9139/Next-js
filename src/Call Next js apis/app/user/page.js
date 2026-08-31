5316a7835b995343c4f7eb3a8d56f2fe03cb8d63
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-31 15:00:08 +0500

    Add user page to fetch and display user list

---
+++ src/Call Next js apis/app/user/page.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+async function getUsers(){
+  let data= await fetch("http://localhost:3000/api/users");
+  data= await data.json();
+  return data;
+}
+
+export default async function Page(){
+
+  const users= await getUsers();
+  console.log(users);
+  
+  return (
+    <div>
+      <h1>User list</h1>
+      {
+        users.map((item)=>{
+         return ( 
+         <div key={item.id}>
+            <Link href={`/user/${item.id}`}>{item.name}</Link>
+          </div>
+          )
+        })
+      }
+    </div>
+  )
+}

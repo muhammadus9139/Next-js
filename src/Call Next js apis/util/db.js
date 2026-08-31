c1609c923b7be23fe5ebd7b995691638112c1e24
Muhammad Usama <muhammadus9139@gmail.com>
2026-08-31 15:01:43 +0500

    Create db.js with sample user data
    Added a user array with sample user data.

---
+++ src/Call Next js apis/util/db.js
@@ -0,0 +1,24 @@
+
+export const user=[
+    {
+        id:1,
+        name:"Usama",
+        age:20,
+        email:"muhammadus9129@gmail.com"
+    },
+
+    {
+        id:2,
+        name:"ali",
+        age:30,
+        email:"ali@gmail.com"
+    },
+
+    {
+        id:3,
+        name:"ahmad",
+        age:10,
+        email:"ahmad@gmail.com"
+    },
+
+]

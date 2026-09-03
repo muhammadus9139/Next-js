2f3d7650d5a2cbafb697917261a33a90d8988b93
Muhammad Usama <muhammadus9139@gmail.com>
2026-09-03 16:42:19 +0500

    Create route.js

---
+++ src/PUT API with mongodb and populate data with api/app/api/products/route.js
@@ -0,0 +1,55 @@
+import { connectionStr } from "@/lib/db";
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+import { Product } from "@/lib/model/product";
+
+export async function GET() {
+  try {
+    await mongoose.connect(connectionStr, {
+      serverSelectionTimeoutMS: 5000,
+      bufferTimeoutMS: 5000,
+    });
+
+    const data = await Product.find();
+
+    return NextResponse.json({ result: data });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: error.message },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request) {
+  try {
+    const payload = await request.json();
+    if (!payload.name || !Number.isFinite(Number(payload.age)) || !payload.email) {
+      return NextResponse.json(
+        { success: false, message: "Name, age, and email are required" },
+        { status: 400 }
+      );
+    }
+
+    await mongoose.connect(connectionStr, {
+      serverSelectionTimeoutMS: 5000,
+      bufferTimeoutMS: 5000,
+    });
+    const product = new Product({
+      name: payload.name,
+      age: Number(payload.age),
+      email: payload.email,
+    });
+
+    const result = await product.save();
+    return NextResponse.json({ success: true, result: result });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 500 }
+    );
+  }
+}

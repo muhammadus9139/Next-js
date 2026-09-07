57c77ba0b800f39ef3fa4f69794d4cd78133bd8a
Muhammad Usama <muhammadus9139@gmail.com>
2026-09-07 17:37:46 +0500

    Add TodoList component to the main page

---
+++ Next js with Redux toolkit/RTK all concepts mini Userlist/src/app/todo/page.js
@@ -0,0 +1,9 @@
+import TodoList from "../components/TodoList";
+
+export default function Page() {
+    return (
+        <main className="users-page">
+            <TodoList />
+        </main>
+    );
+}

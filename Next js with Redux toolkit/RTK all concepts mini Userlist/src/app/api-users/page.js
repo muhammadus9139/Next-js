4c2e409e11174a97c472920274cf1a56679040fa
Muhammad Usama <muhammadus9139@gmail.com>
2026-09-07 17:38:59 +0500

    Add ApiUsers component to users page

---
+++ Next js with Redux toolkit/RTK all concepts mini Userlist/src/app/api-users/page.js
@@ -0,0 +1,9 @@
+import ApiUsers from "../components/ApiUsers";
+
+export default function Page() {
+    return (
+        <main className="users-page">
+            <ApiUsers />
+        </main>
+    );
+}
